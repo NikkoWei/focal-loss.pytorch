332c7547b6b9512aa13370c973fcd3a6e3140676
Mert Bulent Sariyildiz <mbsariyildiz@gmail.com>
2019-03-31 10:07:55 +0200

    Make the code run on PyTorch 1.0 and Python 3.7

--- focalloss_test.py
+++ focalloss_test.py
@@ -1,10 +1,7 @@
-from __future__ import print_function
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 import os,sys,random,time
 import argparse
@@ -15,31 +12,30 @@ from focalloss import *
 start_time = time.time()
 maxe = 0
 for i in range(1000):
-    x = torch.rand(12800,2)*random.randint(1,10)
-    x = Variable(x.cuda())
+    x = torch.rand(12800, 2) * random.randint(1, 10)
+    x = x.cuda()
     l = torch.rand(12800).ge(0.1).long()
-    l = Variable(l.cuda())
+    l = l.cuda()
 
-    output0 = FocalLoss(gamma=0)(x,l)
-    output1 = nn.CrossEntropyLoss()(x,l)
-    a = output0.data[0]
-    b = output1.data[0]
-    if abs(a-b)>maxe: maxe = abs(a-b)
-print('time:',time.time()-start_time,'max_error:',maxe)
+    output0 = FocalLoss(gamma=0)(x, l)
+    output1 = nn.CrossEntropyLoss()(x, l)
+    a = output0.item()
+    b = output1.item()
+    maxe = max(abs(a - b), maxe)
+print('time:',time.time() - start_time,'max_error:',maxe)
 
 
 start_time = time.time()
 maxe = 0
 for i in range(100):
-    x = torch.rand(128,1000,8,4)*random.randint(1,10)
-    x = Variable(x.cuda())
-    l = torch.rand(128,8,4)*1000    # 1000 is classes_num
-    l = l.long()
-    l = Variable(l.cuda())
+    x = torch.rand(128, 1000, 8, 4) * random.randint(1, 10)
+    x = x.cuda()
+    l = torch.rand(128, 8, 4) * 1000    # 1000 is classes_num
+    l = l.long().cuda()
 
-    output0 = FocalLoss(gamma=0)(x,l)
-    output1 = nn.NLLLoss2d()(F.log_softmax(x),l)
-    a = output0.data[0]
-    b = output1.data[0]
-    if abs(a-b)>maxe: maxe = abs(a-b)
-print('time:',time.time()-start_time,'max_error:',maxe)
+    output0 = FocalLoss(gamma=0)(x, l)
+    output1 = nn.NLLLoss()(F.log_softmax(x, dim=1), l)
+    a = output0.item()
+    b = output1.item()
+    maxe = max(abs(a - b), maxe)
+print('time:', time.time() - start_time,'max_error:', maxe)
